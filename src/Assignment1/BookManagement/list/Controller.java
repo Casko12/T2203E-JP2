80a1afab1c8a5ce5b90869ff5ee40e753d1c416b
Casko12 <phamduyhung.ftu@gmail.com>
2022-10-15 08:57:08 +0700

    JP2

--- src/Assignment1/BookManagement/list/Controller.java
+++ src/Assignment1/BookManagement/list/Controller.java
@@ -1,9 +1,13 @@
 package Assignment1.BookManagement.list;
 
+import Assignment1.Main;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -53,6 +57,11 @@ public class Controller implements Initializable {
         }finally {
             tbBooks.setItems(ls);
         }
+    }
+    public void backToHome(ActionEvent actionEvent) throws Exception {
+        Parent listPage = FXMLLoader.load(getClass().getResource("/Assignment1/Home.fxml"));
+        Scene listScene = new Scene(listPage, 800, 600);
+        Main.rootStage.setScene(listScene);
     }
 
 }
--- src/Assignment1/student/list/Controller.java
+++ src/Assignment1/student/list/Controller.java
@@ -1,9 +1,14 @@
 package Assignment1.student.list;
 
+import Assignment1.Main;
 import Assignment1.student.Student;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import Assignment1.student.Student;
@@ -17,31 +22,31 @@ import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    public TableView <Student> tbStudents;
-    public TableColumn <Student, Integer> stID;
+    public TableView<Student> tbStudents;
+    public TableColumn<Student, Integer> stID;
     public TableColumn<Student, String> stName;
     public TableColumn<Student, String> stEmail;
     public TableColumn<Student, String> stPhone;
-    public final static String connectionString ="jdbc:mysql://localhost:3306/t2203e";
-    public final static String user ="root";
-    public final static String pwd ="";
+    public final static String connectionString = "jdbc:mysql://localhost:3306/t2203e";
+    public final static String user = "root";
+    public final static String pwd = "";
     public static ObservableList<Student> list = FXCollections.observableArrayList();
 
     @Override
-    public void initialize (URL location, ResourceBundle resources) {
+    public void initialize(URL location, ResourceBundle resources) {
         stID.setCellValueFactory(new PropertyValueFactory<Student, Integer>("id"));
         stName.setCellValueFactory(new PropertyValueFactory<Student, String>("name"));
         stEmail.setCellValueFactory(new PropertyValueFactory<Student, String>("email"));
         stPhone.setCellValueFactory(new PropertyValueFactory<Student, String>("phone"));
 
         ObservableList<Student> ls = FXCollections.observableArrayList();
-        try{
+        try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection conn = DriverManager.getConnection(connectionString, user, pwd);
             Statement statement = conn.createStatement();
             String sql_txt = "select * from students";
             ResultSet rs = statement.executeQuery(sql_txt);
-            while (rs.next()){
+            while (rs.next()) {
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
                 String email = rs.getString("email");
@@ -49,12 +54,22 @@ public class Controller implements Initializable {
                 Student s = new Student(id, name, email, phone);
                 ls.add(s);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
-        }finally {
+        } finally {
             tbStudents.setItems(ls);
         }
     }
-}
 
+    public void backToHome(ActionEvent actionEvent) throws Exception {
+        Parent listPage = FXMLLoader.load(getClass().getResource("/Assignment1/Home.fxml"));
+        Scene listScene = new Scene(listPage, 800, 600);
+        Main.rootStage.setScene(listScene);
+    }
+    public void addStudent(ActionEvent actionEvent) throws Exception {
+        Parent listPage = FXMLLoader.load(getClass().getResource("Assignment1/student/form/FormStudent.fxml"));
+        Scene listScene = new Scene(listPage, 800, 600);
+        Main.rootStage.setScene(listScene);
+    }
+}
 
