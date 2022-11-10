f140c8904c86eeaddec802b612291a0803e08d4a
Casko12 <phamduyhung.ftu@gmail.com>
2022-11-10 19:22:55 +0700

    Exam

--- src/Exam/student/list/Controller.java
+++ src/Exam/student/list/Controller.java
@@ -1,14 +1,40 @@
 package Exam.student.list;
 
+import Assignment1.entities.Book;
+import Exam.dao.impls.StudentRepository;
+import Exam.entities.Student;
+import Exam.enums.RepositoryType;
+import Exam.factory.RepositoryFactory;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class Controller {
-    public TableColumn tdId;
-    public TableColumn tdName;
-    public TableColumn tdAddress;
-    public TableColumn tdPhone;
-    public TableColumn tdEdit;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public TableView<Student> tbstudents;
+    public TableColumn <Student,String> tdId;
+    public TableColumn <Student,String> tdName;
+    public TableColumn <Student,String> tdAddress;
+    public TableColumn <Student,String> tdPhone;
+    public TableColumn <Student, Button> tdEdit;
+    public static ObservableList<Student> list = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        tdId.setCellValueFactory(new PropertyValueFactory<Student, String>("id"));
+        tdName.setCellValueFactory(new PropertyValueFactory<Student, String>("name"));
+        tdAddress.setCellValueFactory(new PropertyValueFactory<Student, String>("address"));
+        tdPhone.setCellValueFactory(new PropertyValueFactory<Student, String>("phone"));
+        tdEdit.setCellValueFactory(new PropertyValueFactory<Student, Button>("edit"));
+        ObservableList<Student> = (StudentRepository) RepositoryFactory.createRepository(RepositoryType.STUDENT);
+    }
 
     public void addStudent(ActionEvent actionEvent) {
     }
