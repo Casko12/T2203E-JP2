f878e733f2488f35a74c11a8e355d6fb21d9f242
Casko12 <phamduyhung.ftu@gmail.com>
2022-11-10 18:54:40 +0700

    Exam

--- src/Exam/student/list/Controller.java
+++ src/Exam/student/list/Controller.java
@@ -1,4 +1,15 @@
 package Exam.student.list;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+
 public class Controller {
+    public TableColumn tdId;
+    public TableColumn tdName;
+    public TableColumn tdAddress;
+    public TableColumn tdPhone;
+    public TableColumn tdEdit;
+
+    public void addStudent(ActionEvent actionEvent) {
+    }
 }
